d7ecfcdc1ac2da7f8d017d85044af050270a05f1
jma <josephma.kw@gmail.com>
2025-11-17 08:32:15 -0800

    initial commit

---
+++ 01/story.py
@@ -0,0 +1,6 @@
+name = input("Please tpye in a name")
+year = input("Please type in a year")
+
+print(name + " is valiant knight, born in the year " + year + ". One morning "
++ name + " woke up to an awful racket: a dragon was approaching the village. Only " +
+name + " could save the village's residents.")
